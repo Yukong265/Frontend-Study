cdbd766e5181ece61d422ff4d06d0a4128001f68
Yukong265 <subenmoom50@gmail.com>
2023-05-22 16:02:37 +0900

    Refactoring

--- todo/index.js
+++ todo/index.js
@@ -2,7 +2,7 @@ let inputBox = document.getElementById('inputField');  // 할 일 입력창
 let addToDo = document.getElementById('addToDo');      // 버튼
 let toDoList = document.getElementById('toDoList');    // 할 일 리스트창
 
-addToDo.addEventListener('click', function(){    // 버튼에 클릭 이벤트가 발생하면
+addToDo.addEventListener('click', () => {    // 버튼에 클릭 이벤트가 발생하면
     var list = document.createElement('li');     // html 'li' 태그 만들기
     if (!inputBox.value)            // 할 일 입력창에 내용이 입력되지 않으면 alert 발생
         alert('내용을 입력해 주세요!');
@@ -13,10 +13,10 @@ addToDo.addEventListener('click', function(){    // 버튼에 클릭 이벤트�
         inputBox.value= "";               // 할 일 입력창 초기화
     }
 
-    list.addEventListener('click', function(){      // 만들어진 list에 클릭 이벤트가 발생하면 줄 긋기
+    list.addEventListener('click', () => {      // 만들어진 list에 클릭 이벤트가 발생하면 줄 긋기
         list.style.textDecoration = "line-through";
     })
-    list.addEventListener('dblclick', function(){   // list에 더블클릭 이벤트가 발생하면 할 일 리스트창에서 지우기
+    list.addEventListener('dblclick', () => {   // list에 더블클릭 이벤트가 발생하면 할 일 리스트창에서 지우기
         toDoList.removeChild(list);
     })
 })
